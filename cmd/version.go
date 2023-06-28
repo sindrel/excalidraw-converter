6866ac30a1245c1b9c1e3a9eeaeb168a9eb851f0
sindrel <35308882+sindrel@users.noreply.github.com>
2023-06-28 21:09:07 +0200

    fix: Set ldflags in .goreleaser.yaml

--- cmd/version.go
+++ cmd/version.go
@@ -6,15 +6,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version string
-var commit string
+var (
+	version = "dev"
+	commit  = "nnnnnnn"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Output the application version",
 	Long:  `Provides information about the release version and the Git commit it was built from.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("v%s (%s)\n", version, string(commit[0:7]))
+		fmt.Printf("%s (%s)\n", version, string(commit[0:7]))
 	},
 }
 
