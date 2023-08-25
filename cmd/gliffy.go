8b30b2d998532022420bada91c0e2dacf5d9a6e5
sindrel <35308882+sindrel@users.noreply.github.com>
2023-08-25 14:30:48 +0200

    feat: Determine output file name based on input file path

--- cmd/gliffy.go
+++ cmd/gliffy.go
@@ -4,9 +4,13 @@ import (
 	conv "diagram-converter/internal/conversion"
 	"fmt"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+var defaultOutputPath = "your_file.gliffy"
 
 var gliffyCmd = &cobra.Command{
 	Use:   "gliffy",
@@ -19,6 +23,10 @@ var gliffyCmd = &cobra.Command{
 		if len(importPath) == 0 {
 			fmt.Fprintf(os.Stderr, "Input file path not provided. (Use --help for details.)\n")
 			os.Exit(1)
+		}
+
+		if strings.HasPrefix(exportPath, defaultOutputPath) {
+			exportPath = strings.TrimSuffix(path.Base(importPath), ".excalidraw") + ".gliffy"
 		}
 
 		err := conv.ConvertExcalidrawToGliffy(importPath, exportPath)
@@ -34,5 +42,5 @@ func init() {
 	rootCmd.AddCommand(gliffyCmd)
 
 	gliffyCmd.PersistentFlags().StringP("input", "i", "", "input file path")
-	gliffyCmd.PersistentFlags().StringP("output", "o", "output.gliffy", "output file path")
+	gliffyCmd.PersistentFlags().StringP("output", "o", defaultOutputPath, "output file path")
 }
