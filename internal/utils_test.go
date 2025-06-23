d4885e505744105f44aba5fc1c5500f6e2af0a15
sindrel <35308882+sindrel@users.noreply.github.com>
2025-06-23 11:17:47 +0200

    chore: Add test

--- internal/utils_test.go
+++ internal/utils_test.go
@@ -18,3 +18,23 @@ func TestCheckIfLatestVersion(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, isLatest, false)
 }
+
+func TestSanitizeElementText(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"Hello & World", "Hello &amp; World"},
+		{"<tag>", "&lt;tag&gt;"},
+		{"Line1\nLine2", "Line1<br>Line2"},
+		{"<b>&</b>", "&lt;b&gt;&amp;&lt;/b&gt;"},
+		{"NoSpecialChars", "NoSpecialChars"},
+		{"&<>", "&amp;&lt;&gt;"},
+		{"Line1\nLine2\nLine3", "Line1<br>Line2<br>Line3"},
+	}
+
+	for _, tt := range tests {
+		result := SanitizeElementText(tt.input)
+		assert.Equal(t, tt.expected, result, "input: %q", tt.input)
+	}
+}
